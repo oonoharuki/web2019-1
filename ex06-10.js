99d8274f45da4faa0f3599bede5c3bf63702237e
oonoharuki <zm18244@josai.ac.jp>
2019-12-17 03:53:37 +0000

    12/17

--- ex06-10.js
+++ ex06-10.js
@@ -5,16 +5,14 @@ function hyoji(){
     //テキストボックスの文字数
     var cnt = document.timer.moji.value.length;
     //文字が全部表示されているか確認
-}
 if(cnt<11){
     //現在より１文字多く切り出して表示
-    document.timer.moji.value=str.substr(0,cnt+1);}
-    else{
+    document.timer.moji.value=str.substr(0,cnt+1);}else{
         //全て表示されたら、空文字に戻す
-        document.timer.moji.value="";}}
+        document.timer.moji.value="";}
         function startfnc(){
             //関数hyoji()を1000ミリ秒間隔で呼び出す
-            setlnterval("hyoji()",1000);}
+            setInterval ("hyoji()",1000);}
  function mes(){
      alert("3秒経ちました！");
  }            
