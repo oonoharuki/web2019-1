5464b15125435badbb9a3e10a49c6ad4683af5fb
oonoharuki <zm18244@josai.ac.jp>
2019-12-24 04:12:19 +0000

    12/24

--- ex06-8.js
+++ ex06-8.js
@@ -1,10 +1,10 @@
 
 function sayhello68(){
-    alert('Hello,'+documento.getElementById('who68').innerHTML+'!');
+    alert('Hello,'+document.getElementById('who68').innerHTML+'!');
 }
 function taro68(){
-    documento.getElementById('who68').innerHTML='太郎';
+    document.getElementById('who68').innerHTML='太郎';
 }
 function hanako68(){
-    documento.getElementById('who68').innerHTML='花子';
+    document.getElementById('who68').innerHTML='花子';
 }
