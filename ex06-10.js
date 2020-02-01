1674ae527b67e014d49380d4b0d6538a4556e472
oonoharuki <zm18244@josai.ac.jp>
2020-02-01 05:24:30 +0000

    2/1

--- ex06-10.js
+++ ex06-10.js
@@ -13,8 +13,8 @@ else{
 document.timer.moji.value="";}}
        
 function startfnc(){
-//関数hyoji()を1000ミリ秒間隔で呼び出す
-    setInterval ("hyoji()",1000);}
+//関数hyoji()を3000ミリ秒間隔で呼び出す
+    setInterval ("hyoji()",3000);}
 
 function mes(){
 alert("3秒経ちました！");
